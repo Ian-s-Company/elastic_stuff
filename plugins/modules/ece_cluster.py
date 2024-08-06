5c340d7602d11562b32b45e53f7aee0b3315f44b
Ian R Scott <92400549+ianrscottexp@users.noreply.github.com>
2024-08-06 17:50:19 -0500

    Update ece_cluster.py

--- plugins/modules/ece_cluster.py
+++ plugins/modules/ece_cluster.py
@@ -16,7 +16,7 @@ short_description: Create modify or delete Elasticsearch clusters in ECE
 
 version_added: '2.9'
 
-author: Mike Garuccio (@mgaruccio)
+author:  Ian Scott
 
 requirements:
   - python3
